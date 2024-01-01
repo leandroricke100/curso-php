f5313bec6f396839fcf4bdfed7c888dc545aee34
leandroricke100 <leandro_henrique_100@hotmail.com>
2024-01-01 13:03:30 -0300

    update

---
+++ ex003/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>tipos primitivos php</title>
+</head>
+
+<body>
+
+</body>
+
+<h1>Teste de tipos primitivos</h1>
+
+<?php 
+        // $num = 010;
+        // echo "o valor da variavél é $num";
+    
+        // $v = "leandro";
+        // var_dump($v);
+
+        //$num = (int) 5e3;
+        // echo "o valor é $num";
+        //var_dump($num)
+
+        // $num = (float) "950";
+        // var_dump($num);
+
+        $casado = true;
+        //var_dump($casado);
+        print "o valor é $casado";
+    ?>
+
+</html>
